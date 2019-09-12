7c35977d71c5c7acf44c22e6ee67f28ea0a6c5bd
Abdullah Ayub <abdullah@prioritystuff.com>
2019-09-12 13:22:28 +0500

    did some refactoring... also added a few array-based functionalities... code not yet complete...

--- src/main.java
+++ src/main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Menu");
+        System.out.println("Search Elements");
+        System.out.println("Del Elements");
+        System.out.println("modify elements");
+        
         System.out.println("Enter size of the array: ");
         int size = input.nextInt();
         
         khpalaArray obj = new khpalaArray(size);
-        obj.createArray();
+        obj.fillArray();
         obj.printArray();
     }
 }
