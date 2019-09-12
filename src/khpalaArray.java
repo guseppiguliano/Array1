a2db0c029914d90a14193801a264b6d4f5460c3c
Abdullah Ayub <abdullah@prioritystuff.com>
2019-09-12 13:22:28 +0500

    did some refactoring... also added a few array-based functionalities... code not yet complete...

--- src/khpalaArray.java
+++ src/khpalaArray.java
@@ -10,25 +10,39 @@ public class khpalaArray {
         this.size = size;
     }
     
-    public void createArray()
+    public void fillArray()
     {
         list = new int[size];
         Scanner input = new Scanner(System.in);
         
-        for (int i = 0; i < list.length; i++)
-        {
+        for (int i = 0; i < list.length; i++) {
             System.out.print("Enter element[" + i + "]: ");
             list[i] = input.nextInt();
         }
+    }
+    
+    public int searchElement(int key)
+    {
+        
     }
     
     public void printArray()
     {
         System.out.println("Array elements are: ");
         
-        for (int i = 0; i < list.length; i++)
-        {
+        for (int i = 0; i < list.length; i++) {
             System.out.print(list[i] + " ");
+        }
+    }
+    
+    public void deleteElement(int num)
+    {
+        int index = -1;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == num) {
+                index = i;
+                break;
+            }
         }
     }
 }
