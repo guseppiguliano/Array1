d717772139eb8c0581e7edfa08e32a8aaaee7397
Abdullah Ayub <abdullah@prioritystuff.com>
2019-09-15 03:11:42 +0500

    searchElement(int x) function implemented...

--- src/main.java
+++ src/main.java
@@ -4,10 +4,14 @@ public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
+        /*
         System.out.println("Menu");
+        System.out.println("fillArray");
+        System.out.println("printArray");
         System.out.println("Search Elements");
         System.out.println("Del Elements");
         System.out.println("modify elements");
+        */
         
         System.out.println("Enter size of the array: ");
         int size = input.nextInt();
@@ -15,5 +19,9 @@ public class main {
         khpalaArray obj = new khpalaArray(size);
         obj.fillArray();
         obj.printArray();
+        obj.searchElement(47);
+        obj.deleteElement(11);
+//        obj.modifyElement(12);
+//        obj.printArray();
     }
 }
